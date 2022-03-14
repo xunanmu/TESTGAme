e81bc5f5c542cca4e31177fd499e696874db377f
徐南木 <xunanmu@xunanmu.com>
2022-03-14 16:53:55 +0800

    feat(*): 提交工程项目
    1.创建初始工程
    2.创建SphereBase类
    3.为SphereBase绑定相机和键位

---
+++ Source/TESTGame/Private/SphereBase.cpp
@@ -0,0 +1,76 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SphereBase.h"
+
+
+// Sets default values
+ASphereBase::ASphereBase()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	/*默认输出*/
+	bIsInput = true;
+	/*默认初始速度*/
+	SphereSpeed = 200;
+
+	/*球形网格组件*/
+	SphereMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT(__FUNCTION__));
+	SphereMeshComponent->SetSimulatePhysics(true); /*开启物理模拟*/
+
+	/*相机的弹簧臂组件*/
+	CameraArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraArmComponent"));
+	CameraArmComponent->SetupAttachment(SphereMeshComponent);
+	/*关闭相机臂翻转*/
+	CameraArmComponent->bInheritYaw = 0;
+	CameraArmComponent->bInheritPitch = 0;
+	CameraArmComponent->bInheritRoll = 0;
+
+	/*相机组件是附加在弹簧臂组件下面*/
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(CameraArmComponent);
+}
+
+// Called when the game starts or when spawned
+void ASphereBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ASphereBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	/*TODO旧api先学习到时候改成新api SetPhysicsAngularVelocityInDegrees*/
+	if (AngularVector != FVector(0.f))
+	{
+		SphereMeshComponent->SetPhysicsAngularVelocityInDegrees(AngularVector);
+	}
+}
+
+// Called to bind functionality to input
+void ASphereBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	/*绑定按键*/
+	PlayerInputComponent->BindAxis("MoveForward",this,&ASphereBase::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight",this,&ASphereBase::MoveRight);
+}
+
+void ASphereBase::MoveForward(float value)
+{
+	if (bIsInput)
+	{
+		AngularVector.Y = SphereSpeed*value;
+	}
+}
+
+void ASphereBase::MoveRight(float value)
+{
+	if (bIsInput)
+	{
+		AngularVector.X = SphereSpeed*value;
+	}
+}
---
+++ Source/TESTGame/Public/SphereBase.h
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "SphereBase.generated.h"
+
+UCLASS()
+class TESTGAME_API ASphereBase : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ASphereBase();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="SphereMesh")
+	UStaticMeshComponent* SphereMeshComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CameraArm")
+	USpringArmComponent* CameraArmComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	bool bIsInput;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="SphereSpeed")
+	float SphereSpeed;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="SphereSpeedMin")
+	float SphereSpeedMin;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="SphereSpeedMax")
+	float SphereSpeedMax;
+	
+	FVector AngularVector;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintCallable)
+	void MoveForward(float value);
+
+	UFUNCTION(BlueprintCallable)
+	void MoveRight(float value);
+};
